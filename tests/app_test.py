f870d7c9d40c18d3179737c09782736152fe76e4
yunjia <yunjia.hao@mail.utoronto.ca>
2024-09-29 15:22:51 -0400

    up until templates & views

---
+++ tests/app_test.py
@@ -0,0 +1,13 @@
+from project.app import app, init_db
+from pathlib import Path
+
+def test_index():
+    tester = app.test_client()
+    response = tester.get("/", content_type="html/text")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
+
+def test_database():
+    init_db()
+    assert Path("flaskr.db").is_file()
